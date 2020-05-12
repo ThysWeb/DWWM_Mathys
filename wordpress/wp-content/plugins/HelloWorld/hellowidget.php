0c110fe999b326e86282044b07b6db6e475bf908
Thys Web <MathysDevWeb@gmail.com>
2020-05-12 13:55:39 +0200

    hello

--- wordpress/wp-content/plugins/HelloWorld/hellowidget.php
+++ wordpress/wp-content/plugins/HelloWorld/hellowidget.php
@@ -1,21 +1,47 @@
 <?php
 class hellowidget extends WP_Widget
 {
-public function __construct()
-{
-parent::__construct('helloworld', 'Hello World', array('description' => 'Un plug-in
-qui écrit Hello World'));
-}
-public function widget($args, $instance)
-{ // formulaire afficher à l'écran pour l'utilisateur
-// on appel les méthodes standard au cas où un autre plug-in les aurait surchargées
-echo $args['before_widget'];
-echo $args['before_title'];
-echo apply_filters('widget_title', $instance['title']);
-echo $args['after_title'];
-// corps du widget
-?>
-<h1>Hello World</h1>
-<?php
-echo $args['after_widget'];
+    public function __construct()
+    {
+        parent::__construct('helloworld', 'Hello World', array('description' => 'Un plug-in qui écrit Hello World', ));
+    }
+    public function widget($args, $instance)
+    { // formulaire afficher à l'écran pour l'utilisateur
+        // on appel les méthodes standard au cas où un autre plug-in les aurait surchargées
+        echo $args['before_widget'];
+        echo $args['before_title'];
+        echo apply_filters('widget_title', $instance['title']);
+        echo $args['after_title'];
+        // corps du widget
+        $couleur = get_option('helloworld_couleur', 'white');
+        echo '
+            <div id="test" style="color: ' . $couleur . '">Hello World est un plug-in qui enregistre
+            les commentaires en base de données</div>
+                <h1>Hello World</h1>
+                <form action="" method="post">         ' .
+                settings_fields('helloworld_settings') . '
+                    <p>
+                        <label for="helloworld_pseudo">Votre pseudo :</label>
+                        <input style="color:blue;" id="helloworld_pseudo" name="helloworld_pseudo" type="texte"/>
+                        <label for="helloworld_comm">Votre commentaire :</label>
+                        <input style="color:blue;" id="helloworld_comm" name="helloworld_comm" type="texte"/>
+                    </p>
+                    <input type="submit"/>
+                </form>
+            ';
+        echo $args['after_widget'];
+    }
+
+    public function form($instance)
+    {// formulaire de gestion des paramètres pour le module d'administration
+        $title = isset($instance['title']) ? $instance['title'] : '';
+        ?>
+            <p>
+                <label for="<?php echo $this->get_field_name( 'title' ); ?>"><?php _e( 'Title:' ); ?>
+                </label>
+                <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php
+                echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo $title; ?>" />
+            </p>
+        <?php
+    }
 }
--- wordpress/wp-content/plugins/HelloWorld/helloworld.php
+++ wordpress/wp-content/plugins/HelloWorld/helloworld.php
@@ -1,15 +1,20 @@
 <?php
 /*
 Plugin Name: Hello World
-Description: ecrit Hello World
+Description: Plugin qui enregistre les commentaires en bdd
 Author: Martine
 Version: 1.0
-*/
-class HelloWorld_Plugin{
-public function __construct(){
-include_once plugin_dir_path(__FILE__).'/helloClass.php';
-new HelloClass();
-}
+ */
+class HelloWorld_Plugin
+{
+    public function __construct()
+    {
+        include_once plugin_dir_path(__FILE__) . '/helloClass.php';
+        register_activation_hook(__FILE__, array('helloclass', 'installComment'));
+        register_activation_hook(__FILE__, array('helloclass', 'installPseudo'));
+        register_deactivation_hook(__FILE__, array('helloclass', 'uninstall'));
+        new HelloClass();
+        
+    }
 }
 new HelloWorld_Plugin();
-register_activation_hook(__FILE__, array('helloclass', 'install'));
